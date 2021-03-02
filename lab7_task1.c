7750a8fab7746bc9d1304456983c57b642e1be23
Asiya <63673018+aabdrasheva@users.noreply.github.com>
2021-03-02 12:10:12 +0300

    update lab7_task1.c

--- lab7_task1.c
+++ lab7_task1.c
@@ -1,30 +1,20 @@
 #include <sys/types.h>
-
 #include <sys/ipc.h>
-
 #include <sys/shm.h>
-
 #include <stdio.h>
-
 #include <errno.h>
-
 #include <stdlib.h>
 
-int main()
+int main() {
 
-{
-
-  char     *array;   
-
+  char     *array;  
   int     shmid;     
-
   char    pathname[] = "lab7_task2.c";
   key_t   key;     
-  
-int file_size = 0;
-int *int_f;
-FILE *file;
-file = fopen(pathname, "r");
+  int file_size = 0;
+  int *int_f;
+  FILE *file;
+  file = fopen(pathname, "r");
 
 fseek(file, 0L, SEEK_END);
 file_size = ftell(file);
@@ -38,26 +28,9 @@ rewind(file);
 
   }
 
-  if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0) {
-
-    if (errno != EEXIST) {
-
+  if ((shmid = shmget(key, sizeof(int) + file_size*sizeof(char) , 0666|IPC_CREAT)) < 0) {
       printf("Can\'t create shared memory\n");
-
       exit(-1);
-
-    } else {
-
-      if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
-
-        printf("Can\'t find shared memory\n");
-
-        exit(-1);
-
-      }
-
-    }
-
   }
 
   if ((int_f = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
